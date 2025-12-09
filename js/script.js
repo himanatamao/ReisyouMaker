54744413ae6f6e1fcec0d9902b382ed80b76119b
himanatamao <himanatamao@gmail.com>
2025-12-09 22:28:10 +0900

    firstCommit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const inputText = document.querySelector('#inputText');
+const btn = document.querySelector('#btn');
+const returnText = document.querySelector('#returnText')
+
+const letsReisyou = () => {
+    returnText.textContent =
+      `${inputText.value}、${inputText.value}きたあああああああああああああああああ🤣🤣🤣🤣🤣🤣wwwwwwwwwww爆笑イクイクイクイクイクイク🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣その${inputText.value}エグすぎだろって🤪🤪🤪🤪👊👊👊👊${inputText.value}${inputText.value}${inputText.value}${inputText.value}チンポチンポチンポ`;
+
+}
+
+btn.addEventListener('click', letsReisyou);
